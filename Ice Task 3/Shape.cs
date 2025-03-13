0204c3afa3f549c1fa6e09d37477c157156fffca
Perezr12 <ST10448618@vcconnect.edu.za>
2025-03-13 15:33:23 +0200

    Abstract class shape created

---
+++ Ice Task 3/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ice_Task_3
+{
+    abstract class Shape
+    {
+        string name {  get; set; }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Area of {name}= ");
+        }
+    }
+}
